158f27e61e0da6631c4b8dfa54be5607e2f2d45b
Jane Lee <xo930497@yahoo.com.tw>
2023-12-09 19:30:12 -0700

    update folder

--- app/(dashboard)/(routes)/upgrade/page.js
+++ app/(dashboard)/(routes)/upgrade/page.js
@@ -4,10 +4,7 @@ import UploadForm from './_components/UploadForm'
 
 function Upgrade() {
   return (
-    <div className='p-5 px-8 md:px-28'>
-        <h2 className='text-[20px] text-center m-5'>Let's <strong className='text-primary'>upload</strong> file and <strong className='text-primary'>share</strong> it</h2>
-        <UploadForm/>
-    </div>
+    <div>Upgrade</div>
   )
 }
 
--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Upload() {
   return (
-    <div>Upload</div>
+    <div className='p-5 px-8 md:px-28'>
+        <h2 className='text-[20px] text-center m-5'>Let's <strong className='text-primary'>upload</strong> file and <strong className='text-primary'>share</strong> it</h2>
+        <UploadForm/>
+    </div>
   )
 }
 
