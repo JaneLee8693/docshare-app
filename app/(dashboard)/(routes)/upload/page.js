53fce6e3cdd9054be6f6507fddf3ebe5689f0bee
Jane Lee <xo930497@yahoo.com.tw>
2023-12-11 02:50:36 -0700

    Firestore setup and upload config

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,14 +1,19 @@
 "use client"
 import React, { useState } from 'react'
 import UploadForm from './_components/UploadForm'
+import { useUser } from '@clerk/nextjs'
 import { app } from '../../../../firebase.config'
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
-
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { doc, getFirestore, setDoc } from 'firebase/firestore'
+import { generateRandomString } from './../../../_utils/GenerateRandomString'
 
 function Upload() {
-
+  const { user } = useUser()
   const [progress,setProgress]=useState()
   const storage = getStorage(app)
+  const db = getFirestore(app)
+  const [fileDocId,setFileDocId]=useState()
+  
   const uploadFile = (file) => {
     // const metadata = {
     //   contentType: file.type
@@ -23,8 +28,26 @@ function Upload() {
       setProgress(progress)
       progress == 100 && getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
         console.log('File available at', downloadURL);
+        saveInfo(file,downloadURL);
       });
     },)
+  }
+
+  const saveInfo = async(file,fileUrl)=>{
+    const docId = generateRandomString().toString();;
+    setFileDocId(docId)
+    await setDoc(doc(db, "uploadedFile", docId), {
+      fileName:file?.name,
+      fileSize:file?.size,
+      fileType:file?.type,
+      fileUrl:fileUrl,
+      userEmail:user?.primaryEmailAddress.emailAddress,
+      userName:user?.fullName,
+      password:'',
+      id:docId,
+      shortUrl:process.env.NEXT_PUBLIC_BASE_URL + docId
+    });
+    
   }
 
   return (
