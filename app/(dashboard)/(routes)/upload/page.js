ecba05cd9d8d210becc4881ff14de54ef9388084
Jane Lee <xo930497@yahoo.com.tw>
2023-12-11 03:07:13 -0700

    Add upload validation

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,19 +1,22 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import UploadForm from './_components/UploadForm'
+import CompleteCheck from './_components/CompleteCheck'
 import { useUser } from '@clerk/nextjs'
 import { app } from '../../../../firebase.config'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import { doc, getFirestore, setDoc } from 'firebase/firestore'
 import { generateRandomString } from './../../../_utils/GenerateRandomString'
+import { useRouter } from 'next/navigation'
 
 function Upload() {
   const { user } = useUser()
+  const router=useRouter()
   const [progress,setProgress]=useState()
   const storage = getStorage(app)
   const db = getFirestore(app)
   const [fileDocId,setFileDocId]=useState()
-  
+  const [uploadCompleted,setUploadCompleted]=useState(false)
   const uploadFile = (file) => {
     // const metadata = {
     //   contentType: file.type
@@ -47,13 +50,31 @@ function Upload() {
       id:docId,
       shortUrl:process.env.NEXT_PUBLIC_BASE_URL + docId
     });
-    
   }
 
+  useEffect(()=>{
+    console.log("Trigger")
+    
+    progress==100&& setTimeout(()=>{
+      setUploadCompleted(true);
+    },2000)
+  },[progress==100]);
+
+  useEffect(()=>{
+    uploadCompleted&&
+    setTimeout(()=>{
+      setUploadCompleted(false);
+      console.log("FileDocId",fileDocId)
+      // router.push('/file-preview/'+fileDocId);
+    },2000)
+  },[uploadCompleted==true])
+
   return (
-    <div className='p-5 px-8 md:px-28'>
+    <div className='p-5 px-8 md:px-28 text-center'>
+      {!uploadCompleted?<div>
         <h2 className='text-[20px] text-center m-5'>Let's <strong className='text-primary'>upload</strong> file and <strong className='text-primary'>share</strong> it</h2>
         <UploadForm uploadClick={(file) => uploadFile(file)} progress={progress}/>
+      </div> : <CompleteCheck/>}
     </div>
   )
 }
